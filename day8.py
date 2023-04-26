61967aeca8f4949f339308fa1ccb14d2240d6f60
Christian  Baun <baunbaun@gmail.com>
2023-04-26 23:08:18 +0200

    Day 8 part 2 - WIP

--- day8.py
+++ day8.py
@@ -1,8 +1,8 @@
 from common import getData, printLst, removeNewLine
 import numpy as np
 
-data = removeNewLine(getData(__file__))
-#data = removeNewLine(getData(__file__, 'test'))
+#data = removeNewLine(getData(__file__))
+data = removeNewLine(getData(__file__, 'test'))
 
 # PART 1 ###################################################
 
@@ -59,7 +59,67 @@ def countVisibleTrees(forrest):
 
     return boolAll.sum()
 
-print('Visible trees: ', countVisibleTrees(npData))
+#print('Visible trees: ', countVisibleTrees(forrest=npData))
 
 # PART 2 ###################################################
 
+def getScenicScoreForTreesInLine(treeLine):
+    treeScores = []
+    for i in range(len(treeLine)):
+        tree = treeLine[i]
+        treeScores.append(getScenicScoreForOneTree(tree=tree, index=i, treeLine=treeLine))
+    return treeScores
+
+def getScenicScoreForOneTree(tree:int, index:int, treeLine:list):
+    lookLeft = list(treeLine[:index])
+    lookLeft.reverse()
+    lookRight = list(treeLine[index+1:])
+    return getScenicScoreOneDirection(tree, lookLeft) * getScenicScoreOneDirection(tree, lookRight)
+
+def getScenicScoreOneDirection(tree:int, treeLineSegment:list):
+    visibleTreesSum = 0
+    for t in treeLineSegment:
+        visibleTreesSum = visibleTreesSum + 1
+        
+        if int(t) >= int(tree): # DER MANGLER AT TAGES HENSYN TIL TRÆ SOM SKYGGER FOR SMÅ TRÆER.ß
+            break   
+
+    return visibleTreesSum
+
+def getScenicScore(row, column, forrest):
+    tree = int(forrest[row][column])  
+    
+    verticalTreeLine = np.rot90(np.asarray(forrest))[column]
+    verticalScore = getScenicScoreForOneTree(tree, row, verticalTreeLine)
+    horizontalScore = getScenicScoreForOneTree(tree, column, forrest[row])
+    
+    return horizontalScore * verticalScore
+
+def getAllScenicScores(forrest):
+    w = len(forrest[0])
+    h = len(forrest)
+
+    scenicScores = []
+
+    for row in range(h):
+        for column in range(w):
+            score = getScenicScore(row, column, forrest)
+            print('(x,y) = score', column, row, score)
+            scenicScores.append(score)
+    return scenicScores
+
+printLst(npData)
+print(np.max(getAllScenicScores(npData)))
+
+# TESTING ###################################################
+i = 2
+treeLine = '33549'
+tree = treeLine[i]
+
+#score = getScenicScore(3,2,npData)
+#print(score)
+
+#print(treeLine)
+#score = getScenicScoreForOneTree(tree=tree, index=i, treeLine=treeLine)
+
+#print(score)
